6fad1ddf3b225e1dbf6b76d2ad8e0dd9ac35c389
shanedabes <shane@isda.best>
2020-12-20 03:46:56 +0000

    add force git pull script

---
+++ force-sync.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+# simple script used to force a git pull in the home assistant container
+
+kubectl exec -it $(kubectl get po -l app.kubernetes.io/name=home-assistant -o name -A) -c home-assistant -- git pull
