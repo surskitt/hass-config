2fa34b79c93d1694c85898799111b0c28a543105
shanedabes <shane@isda.best>
2020-12-20 11:59:06 +0000

    add names to bathroom sensor cards

--- force-sync.sh
+++ force-sync.sh
@@ -2,4 +2,4 @@
 
 # simple script used to force a git pull in the home assistant container
 
-kubectl exec -it $(kubectl get po -l app.kubernetes.io/name=home-assistant -o name -A) -c home-assistant -- git pull --ff
+kubectl exec -it $(kubectl get po -l app.kubernetes.io/name=home-assistant -o name -A) -c home-assistant -- git pull --ff -X theirs
