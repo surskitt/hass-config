f0e7d2de3ec0fd1fe24de48b8fc1d347786b051b
shanedabes <shane@isda.best>
2020-12-21 01:27:36 +0000

    checkout to reset files before synching git

--- force-sync.sh
+++ force-sync.sh
@@ -2,4 +2,5 @@
 
 # simple script used to force a git pull in the home assistant container
 
+kubectl exec -it $(kubectl get po -l app.kubernetes.io/name=home-assistant -o name -A) -c home-assistant -- git checkout .
 kubectl exec -it $(kubectl get po -l app.kubernetes.io/name=home-assistant -o name -A) -c home-assistant -- git pull --ff -X theirs
