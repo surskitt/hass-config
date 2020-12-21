45a4091b5727afcfcd332cdabd8ac86da4a8201a
shanedabes <shane@isda.best>
2020-12-21 01:27:15 +0000

    add copy script to copy files into container

---
+++ copy.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+if [[ "${#@}" -eq 0 ]]; then
+    echo "Usage: ${0} FILE"
+    exit 1
+fi
+
+F="${1}"
+
+POD=$(kubectl get po -l app.kubernetes.io/name=home-assistant -o jsonpath="{.items[0].metadata.name}" -A)
+
+kubectl cp "${F}" "${POD}:${F}" -c home-assistant
