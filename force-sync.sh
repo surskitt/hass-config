1192613cadd7cd8834850b9144e189af682690ed
shanedabes <shane@isda.best>
2020-12-23 01:27:51 +0000

    remove all local chanegs when pulling

--- force-sync.sh
+++ force-sync.sh
@@ -2,5 +2,6 @@
 
 # simple script used to force a git pull in the home assistant container
 
-kubectl exec -it $(kubectl get po -l app.kubernetes.io/name=home-assistant -o name -A) -c home-assistant -- git checkout .
-kubectl exec -it $(kubectl get po -l app.kubernetes.io/name=home-assistant -o name -A) -c home-assistant -- git pull --ff -X theirs
+kubectl exec -it $(kubectl get po -l app.kubernetes.io/name=home-assistant -o name -A) -c home-assistant -- git fetch
+kubectl exec -it $(kubectl get po -l app.kubernetes.io/name=home-assistant -o name -A) -c home-assistant -- git reset --hard HEAD
+kubectl exec -it $(kubectl get po -l app.kubernetes.io/name=home-assistant -o name -A) -c home-assistant -- git merge '@{u}'
